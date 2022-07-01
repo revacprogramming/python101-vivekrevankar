9c41554e01bfa7d1012875b310c0c66c92b7c825
vivekrevankar <97089335+vivekrevankar@users.noreply.github.com>
2022-07-01 09:17:55 +0530

    Update problem#13.py

--- ActivitySet#1/problem#13.py
+++ ActivitySet#1/problem#13.py
@@ -1,3 +1,26 @@
+dicts = dict()
+lst = list()
+fname = input('enter the file name:')
+try:
+    fhand = open(fname)
+except:
+    print('file cannoted be opened',fname)
+    
+for line in fhand:
+    words = line.split()
+    if len(words) < 3 or words[0] != 'From':
+        continue
+        
+    pos = words[5].find(':')
+    hour = words[5][:pos]
+    if hour not in dicts:
+        dicts[hour] = 1
+    else:
+        dicts[hour] += 1
+        
+for key, val in list(dicts.items()):
+    lst.append((key,val))
 
-
-print("hello world")
+lst.sort()
+for key, val in lst:
+    print(key,val)
