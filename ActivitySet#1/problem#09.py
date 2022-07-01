f473f0d2d63140358faee7008bd43a370c617f5c
vivekrevankar <97089335+vivekrevankar@users.noreply.github.com>
2022-07-01 09:21:46 +0530

    Update problem#09.py

--- ActivitySet#1/problem#09.py
+++ ActivitySet#1/problem#09.py
@@ -1,5 +1,18 @@
-print("hello world")
-
-
-
-
+fname = input("Enter file name: ")
+try:
+    fhand = open(fname)
+except:
+    print("the file annot be opened", fname)
+    quit()
+count = 0
+total = 0
+for line in fhand:
+    if line.startswith('X-DSPAM-Confindence:'):
+        count = count + 1
+        pos = find(':')
+        number = line[pos +1:].strip
+        num = float(number)
+        total = total + num
+    
+average = total / count
+print("Average spam confidence:", average)
