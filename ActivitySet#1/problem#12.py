4160dee5f00ba6342c56aced4921dbc57b3eef9b
vivekrevankar <97089335+vivekrevankar@users.noreply.github.com>
2022-07-01 09:35:32 +0530

    Update problem#12.py

--- ActivitySet#1/problem#12.py
+++ ActivitySet#1/problem#12.py
@@ -1,3 +1,25 @@
-# Regular Expressions
-# https://www.py4e.com/lessons/regex
-print("hello world")
+dicts = dict()
+fname = input('enter the file:')
+try:
+    fhand = open(fname)
+except:
+    print('file cannot be opened:', fname)
+    quit()
+    
+for line in fhand:
+    words = line.split()
+    if len(words) < 3 or words[0] != 'From':
+        continue
+    if words[1] not in  dicts:
+        dicts[words[1]] = 1
+    else:
+        dicts[words[1]] += 1
+        
+bigcount = None
+bigword = None
+for word,count in dicts.items(): 
+    if bigcount is None or count > bigcount:
+        bigcount = count
+        bigword = word
+
+print (bigword,bigcount)
