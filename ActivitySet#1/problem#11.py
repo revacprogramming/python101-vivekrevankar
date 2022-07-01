bbb96f3e9faa8a6b7725d969a5e834189b9bc3df
vivekrevankar <97089335+vivekrevankar@users.noreply.github.com>
2022-07-01 09:24:20 +0530

    Update problem#11.py

--- ActivitySet#1/problem#11.py
+++ ActivitySet#1/problem#11.py
@@ -1,4 +1,9 @@
-
-print("hello world")
-
-
+fhand = open('mbox-short.txt')
+count = 0
+for line in fhand:
+    words = line.split()
+    if len(words) < 3 or words[0] != 'From':
+        continue
+    print(words[1])
+    count = count + 1
+print("There were %d lines in the file with From as the first word" % count)
