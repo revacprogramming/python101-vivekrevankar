bbd9d9e6ea70dfb126411d5251211b1d530ec6c7
vivekrevankar <97089335+vivekrevankar@users.noreply.github.com>
2022-07-05 15:42:29 +0530

    Update problem#14.py

--- ActivitySet#1/problem#14.py
+++ ActivitySet#1/problem#14.py
@@ -1,1 +1,16 @@
-aa
+import re
+
+count = 0                              
+
+input_exp = input('Enter a regular expression: ')
+reg_exp = str(input_exp)              
+fname = 'mbox.txt'
+fhand = open(fname)
+
+for line in fhand:
+    line = line.rstrip()
+
+    if re.findall(reg_exp, line) != []:
+        count += 1
+
+print(fname + ' had ' + str(count) + ' lines that matched ' + reg_exp)
