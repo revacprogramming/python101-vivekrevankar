9609b01050e0af32e6e314a294becc3d39959c52
vivekrevankar <97089335+vivekrevankar@users.noreply.github.com>
2022-07-08 03:41:55 +0000

    a

--- ActivitySet#1/problem#15.py
+++ ActivitySet#1/problem#15.py
@@ -1,3 +1,13 @@
-# Object Oriented Programming
-# https://www.py4e.com/lessons/Object
-ab
+from socket import *
+
+mysock = socket(AF_INET, SOCK_STREAM)
+mysock.connect(('data.pr4e.org', 80))
+cmd = 'GET http://data.pr4e.org/intro-short.txt HTTP/1.0\r\n\r\n'.encode()
+mysock.send(cmd)
+
+while True:
+    data = mysock.recv(512)
+    if (len(data) < 1):
+        break
+    print(data.decode())
+mysock.close()
